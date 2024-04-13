e79b07ee993549f76de6541838d8582065dbae2d
coleYab <yabume123@gamil.com>
2024-04-13 12:23:49 +0300

    changed square to new thing

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Documentations """
 
 
-class Square():
+class square():
     """ Documentations """
 
     def __init__(self, *args, **kwargs):
@@ -14,22 +14,22 @@ class Square():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the Square """
+        """ Area of the square """
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-    """ Documentations """
+        """ Documentations """
 
-    return (self.width * 2) + (self.height * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-    """ Documentations """
+        """ Documentations """
 
-    return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
