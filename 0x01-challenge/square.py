8030989d6604cf8ebdb5314506ccc029feb27bcb
coleYab <yabume123@gamil.com>
2024-04-16 19:57:59 +0300

    Added files

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" Module """
+""" Defines Square class."""
 
-class square():
-    """ Class square """
+
+class Square():
+    """ Square class."""
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        self.width = 0
-        self.height = 0
-        """ constructor """
+        """ Instantiation of class."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,17 +16,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ perimeter of quare """
+    def permiter_of_my_square(self):
+        """ Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ string representation """
+        """ Printable representation of square object."""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    """ Creates Square object."""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-User class
-"""
+""" User class """
+
 
 class User():
     """ Documentation """
